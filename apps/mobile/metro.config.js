5a475198918fc3dc5844597bf76371b7c889808a
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-09-27 18:55:45 +0100

    fix app metro config

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -23,13 +23,13 @@ const monorepoPackages = {
 config.resolver.sourceExts.push('cjs');
 
 // 1. Watch the local app folder, and all related packages (limiting the scope and speeding it up)
-// config.watchFolders = [workspaceRoot]
-config.watchFolders = [__dirname, ...Object.values(monorepoPackages)];
+config.watchFolders = [workspaceRoot]
+// config.watchFolders = [__dirname, ...Object.values(monorepoPackages)];
 // Add the monorepo workspaces as `extraNodeModules` to Metro.
 // If your monorepo tooling creates workspace symlinks in the `node_modules` folder,
 // you can either add symlink support to Metro or set the `extraNodeModules` to avoid the symlinks.
 // See: https://facebook.github.io/metro/docs/configuration/#extranodemodules
-config.resolver.extraNodeModules = monorepoPackages;
+// config.resolver.extraNodeModules = monorepoPackages;
 
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
