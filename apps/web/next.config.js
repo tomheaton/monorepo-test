a3205e549e7e500addf964fae4d29518087a1703
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-11-08 22:43:52 +0000

    revert back to next-12

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,14 +1,20 @@
+const withTM = require('next-transpile-modules')([
+    "@monorepo-test/api",
+    "@monorepo-test/react",
+    "@monorepo-test/utils"
+]);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    experimental: {
+    /*experimental: {
         transpilePackages: [
             "@monorepo-test/api",
             "@monorepo-test/react",
             "@monorepo-test/utils"
         ],
-    },
+    },*/
 }
 
-module.exports = nextConfig;
+module.exports = withTM(nextConfig);
