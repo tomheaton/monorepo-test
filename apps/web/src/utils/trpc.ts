f89596c1e14b1271f04cb9ff4850032ce00ccae0
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-11-28 02:57:30 +0000

    small additions

--- apps/web/src/utils/trpc.ts
+++ apps/web/src/utils/trpc.ts
@@ -31,12 +31,20 @@ export const trpc = createTRPCNext<AppRouter>({
             (opts.direction === "down" && opts.result instanceof Error),
         }),
         httpBatchLink({
+          /**
+           * If you want to use SSR, you need to use the server's full URL
+           * @link https://trpc.io/docs/ssr
+           **/
           url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
+      /**
+       * @link https://react-query-v3.tanstack.com/reference/QueryClient
+       **/
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
     };
   },
-  ssr: false,
+  ssr: true,
 });
 
 export type {RouterInputs, RouterOutputs};
